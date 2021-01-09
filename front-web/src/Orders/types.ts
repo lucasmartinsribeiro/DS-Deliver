671b6801f5ce586807de70a5ed1da5f4bff2713e
lucas <lucasmartins0498@gmail.com>
2021-01-09 19:57:04 -0300

    Finish front-web

--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -4,10 +4,18 @@ export type Product = {
     price: number;
     description: string;
     imageUri: string;
-  }
+}
 
 export type OrderLocationdata = {
   latitude: number;
   longitude: number;
   address: string;
 }
+
+type ProductId = {
+  id: number
+}
+
+export type OrderPayLoad = {
+  products: ProductId[],
+} & OrderLocationdata
